9a2c167d2adf480eb867213034347ffd99e907df
jerrygerou <jerrysgerou@gmail.com>
2024-12-03 18:32:59 -0600

    Part 2, Day 3. Finally

--- 2024/12-03/12-03.rb
+++ 2024/12-03/12-03.rb
@@ -1,22 +1,55 @@
 class Multiplier
-    attr_accessor :total, :building_array
+    attr_accessor :total, :building_array, :enabled
     attr_reader :file, :input
-    def initialize(input_filepath)
+
+    START_STRING = "do()"
+    END_STRING = "don't()"
+    def initialize(input_filepath, enabled=false)
         @file = File.open(input_filepath)
         @input = file.readlines.map(&:strip)
         @building_array = []
         @result_array = []
+        @enabled = enabled
     end
 
     def solve
-        scan_and_gather
+        if @enabled
+            extract_and_shrink!(@input.join(''))
+        else
+            regex_matcher(@input.join(''))
+        end
         multiply_and_total
         puts(@result_array.sum)
     end
 
-    def scan_and_gather
-        @input.each do |row|
-            regex_matcher(row)
+    # def scan_and_gather
+    #
+    #     @input.each do |row|
+    #         regex_matcher(row)
+    #     end
+    # end
+
+    def extract_and_shrink!(input_string)
+        target = @enabled ? END_STRING : START_STRING
+        last_run = false
+        target_index = input_string.index(target)
+
+        if target_index
+            extracted = input_string[0..(target_index + target.length - 1)]
+            remaining = input_string[(target_index + target.length)..-1]
+        else
+            puts "No target index found"
+            last_run = true
+            extracted = input_string
+        end
+
+        if target == END_STRING
+            regex_matcher(extracted)
+        end
+
+        @enabled = !enabled
+        unless last_run
+            extract_and_shrink!(remaining)
         end
     end
 
@@ -47,3 +80,4 @@ class Multiplier
 end
 
 puts(Multiplier.new('input_file.txt').solve)
+puts(Multiplier.new('input_file.txt', true).solve)
