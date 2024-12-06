e13cc24510293d425744fe700633e0922e19a664
jerrygerou <jerrysgerou@gmail.com>
2024-12-06 06:03:38 -0600

    Day 5, part 1

---
+++ 2024/12-05/12-05.rb
@@ -0,0 +1,65 @@
+class PageOrderer
+    attr_reader :file, :input
+    attr_accessor :rules, :page_numbers, :solutions
+    def initialize(input_filepath)
+        @file = File.open(input_filepath)
+        @input = file.readlines.map(&:split)
+        @rules = []
+        @page_numbers = []
+        @solutions = []
+    end
+
+    def solve
+        prepare_sections
+        solve_problem
+    end
+
+    def prepare_sections
+        @input.each do |line|
+            if line.nil? || line.empty?
+                next
+            end
+            if line[0].include?('|')
+                @rules << line[0].split('|').map(&:to_i)
+            else
+                @page_numbers << line[0].split(',').map(&:to_i)
+            end
+        end
+    end
+
+
+    def valid_update?(update, rules)
+        rules.all? do |x, y|
+            if update.include?(x) && update.include?(y)
+                update.index(x) < update.index(y) # x must come before y
+            else
+                true
+            end
+        end
+    end
+
+    def solve_problem
+        valid_updates = []
+        page_numbers.each do |update|
+            valid = true
+            rules.each do |left, right|
+                if update.include?(left) && update.include?(right)
+                    unless update.index(left) < update.index(right)
+                        valid = false
+                        break
+                    end
+                end
+            end
+            valid_updates << update if valid
+        end
+
+        middle_pages = []
+        valid_updates.each do |update|
+            middle_pages << update[update.length / 2]
+        end
+
+        middle_pages.sum
+    end
+end
+
+puts(PageOrderer.new('input.txt').solve)
